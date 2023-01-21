4de03260256da8bc2d7419208962389a2312798c
quirogak <quirogakill@gmail.com>
2023-01-21 12:46:14 -0500

    Sorted every recursion of the given array. And then returned the full array organized properly.

--- MergeSort/script.js
+++ MergeSort/script.js
@@ -1,58 +1,37 @@
 
 const mergeSort = (arr) => {
 
-  let leftSide = arr.slice(0,arr.length/2)
-  let rightSide = arr.slice(arr.length/2,arr.length)
-
-  const totalContainer = []
-    totalContainer.push(leftSide)
-    totalContainer.push(rightSide)
-
-    console.log(totalContainer)
-
-
-  if ((leftSide.length == 1 && rightSide.length == 1) ||(leftSide.length == 0 && rightSide.length == 1) ){
-
-    return totalContainer
-
-  }
-
-  else {
-    
-   return mergeSort(leftSide) + mergeSort(rightSide)
-  }
-        
-
-    }
-
-
-console.log(mergeSort([3,1,2,4,4,2,1,2,2]))
+let leftSide = arr.slice(0,arr.length/2)
+let rightSide = arr.slice(arr.length/2,arr.length)
+ 
+ let totalContainer = []
+ totalContainer.push(leftSide)
+ totalContainer.push(rightSide)
 
   
-
-
-    
-
-
-
-   
-       
-        
-
+  if ((leftSide.length == count && rightSide.length == count) ||(leftSide.length == count-1 && rightSide.length == count) ) return totalContainer
      
+  else {
+
+    let sorted = [mergeSort(leftSide) + mergeSort(rightSide)]
+    let split = sorted[0].replaceAll(",", "")
+    let newArray = split.split("")
+  
+    for (let i = 1; i < newArray.length; i++)
+    for (let j = 0; j < i; j++)
+        if (newArray[i] < newArray[j]) {
+          let copy = newArray[i];
+          newArray[i] = newArray[j];
+          newArray[j] = copy;
+        }
+
+   return newArray
+
+  }
+}
 
 
-     
-        
-
-    
-    
-
-
-
-
-
-
+console.log(mergeSort([3,1,4,2,4,2,2,1]))
 
 
 
